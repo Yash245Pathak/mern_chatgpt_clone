2415223e0dec778425d27c25838f8d768d3de486
Yash Pathak <90676914+Yash245Pathak@users.noreply.github.com>
2023-04-03 08:44:27 +0530

    final update

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
     setInput("");
     setChat(chatNew)
     const messages = chatNew.map((message) => message.message).join("\n");
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://mern-chatgpt-clone-openai.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
